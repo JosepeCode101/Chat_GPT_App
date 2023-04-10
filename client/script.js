76cea2be4a9a62bb679ae5f10b693ff4858806dd
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2023-04-10 07:48:26 +0200

    Server finally compile

--- client/script.js
+++ client/script.js
@@ -45,7 +45,7 @@ function chatStripe (isAi, value, uniqueId) {
   return (
     `
     <div class="wrapper ${isAi && 'ai'}">
-      <div className="profile">
+      <div class="profile">
         <img
           src="${isAi ? bot : user}"
           alt="${isAi ? 'bot' : 'user'}"
@@ -79,6 +79,31 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
+  const response = await fetch('http://localhost:5000', {
+    methot: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  if(response.ok) {
+    const data = await response.json();
+    const parseData = data.bot.trim();
+
+    typeText(messageDiv, parseData)
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
 }
 
 form.addEventListener('submit', handleSubmit);
